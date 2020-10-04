ae8005497a645a943e7c9b8ccaffe69f3031c86d
Eduardo Vinícius Rocha <eduardo.vinicius.rocha@gmail.com>
2020-10-04 19:38:47 -0300

    start work with Devise Views

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,9 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack")
 
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+    $: 'jquery',
+    JQuery: 'jquery',
+    Popper: ['popper.js', 'default', 'toast']
+}))
 module.exports = environment
